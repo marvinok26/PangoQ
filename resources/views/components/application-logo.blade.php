a342aa793acde0f7368e5be1e642b6caee634bd4
Marvin Okongo <okongomarvin971@gmail.com>
2025-06-09 12:16:38 +0300

    declutter

--- resources/views/components/application-logo.blade.php
+++
@@ -1,5 +0,0 @@
-{{-- resources/views/components/application-logo.blade.php --}}
-<a href="{{ route('home') }}" class="flex items-center">
-    <div class="h-8 w-8 bg-secondary-600 rounded-full"></div>
-    <span class="ml-2 text-xl font-bold text-secondary-600">pangoQ</span>
-</a>
--- resources/views/components/button.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- resources/views/components/button.blade.php --}}
-@props(['type' => 'submit'])
-
-<button type="{{ $type }}" {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-secondary-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-secondary-700 active:bg-secondary-900 focus:outline-none focus:border-secondary-900 focus:ring ring-secondary-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
